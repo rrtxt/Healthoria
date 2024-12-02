3295bbce3a4374035b4adfb2c0b9d0748922d819
rrtxt <rizfiferdian1@gmail.com>
2024-12-02 10:26:34 +0700

    add QuestCard

--- src/components/quest/QuestCard.jsx
+++ src/components/quest/QuestCard.jsx
@@ -16,14 +16,22 @@ const QuestCard = () => {
         />
         <h3 className=" font-sourceSans3">AP +6</h3>
       </div>
-      <div className="bg-white2 w-[70%]">
+      <div className="bg-white2 w-[70%] rounded-r-[9px]">
         <h3 className="font-sourceSans3 font-semibold text-[16px] my-[6px] ml-[8px] text-black">
           Run 2 Km
         </h3>
         <div className="h-[2px] bg-blue2 pr-[16px]"></div>
-        <p className="text-black font-sourceSans3 text-[12px]">
+        <p className="text-black font-sourceSans3 text-[12px] mt-[5px] ml-[8px]">
           Progress 2 Km / 2 Km
         </p>
+        <div className="flex items-end justify-end">
+          <div
+            className="font-sourceSans3 mr-[16px] bg-blue2 
+          flex items-center justify-center w-[66px] h-[23px]
+          rounded-tr-[2px] rounded-tl-[9px] rounded-br-[9px] rounded-bl-[2px]">
+            Claim
+          </div>
+        </div>
       </div>
     </div>
   );
