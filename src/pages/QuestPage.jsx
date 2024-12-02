44f41c1cab23dedbe9a683107450f3725f16dd9d
rrtxt <rizfiferdian1@gmail.com>
2024-12-02 10:27:05 +0700

    add QuestCard item

--- src/pages/QuestPage.jsx
+++ src/pages/QuestPage.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import QuestCard from "../components/quest/QuestCard";
 
 const QuestPage = () => {
+  const [quests, setQuests] = useState([0, 1, 2]);
   return (
     <div>
       <h1 className="font-sourceSans3 font-semibold italic text-black ml-[32px] mt-[15px]">
         Daily Quest
       </h1>
       <ul className="mt-[28px] flex flex-col justify-center items-center">
-        <li>
-          <QuestCard />
-        </li>
+        {quests.map((quest, index) => {
+          return (
+            <li key={index} className="mt-[12px]">
+              <QuestCard />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
